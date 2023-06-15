28f8f799f4ed6a5d76218ac4cc6f6ecf743ae25a
nslly <islegulpo@gmail.com>
2023-06-15 11:32:46 +0800

    Set the mutator accessor into latest laravel

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Question extends Model
@@ -21,10 +22,21 @@ class Question extends Model
         $this->belongsTo(User::class);
     }
 
-    public function setTitleAttribute($value)
-    {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = Str::slug($value);
+    // public function setTitleAttribute($value)
+    // {
+    //     $this->attributes['title'] = $value;
+    //     $this->attributes['slug'] = Str::slug($value);
     
+    // }
+
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => [
+                'title' => $value,
+                'slug'  => Str::slug($value)
+            ]
+        );
     }
+
 }
